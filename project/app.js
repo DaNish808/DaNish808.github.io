c4ea2b14fd592003ec12af19f31297ffb7395600
David Nishikawa <triptype@gmail.com>
2017-08-02 16:12:04 -0700

    added scoring system

--- project/app.js
+++ project/app.js
@@ -4,86 +4,98 @@ var want = confirm("Feel like trying a get-to-know-the-author quiz?");
 
 // QUIZ
 if(want){
-    // console.log('user is taking the quiz: ');
-    // alert('Alright, we\'ll start with some yes or no questions.');
 
-    // // Q's and A's for Y/N quiz
-    // // questions [i, 0], true response [i, 1], false response [i, 2]
-    // var yn_qNrs = [ [ 'I\'m 25 years old',                   // question 1
-    //                 'That\' right!',                         // response 1 if answer === true
-    //                 'I am!' ],                              // response 1 if answer === false
-    //                 [ 'I go hiking with my cat.',
-    //                 'His name is Oreo!',
-    //                 'You\'re right! I bring my dog too.' ],
-    //                 [ 'When I was 7 I told my mom I wanted to be an engineer.',
-    //                 'She wishes I did. I told her I\'d become an entomologyst ^^',
-    //                 'Correct!' ],
-    //                 [ 'I practice Muay Thai.',
-    //                 'Not anymore :(',
-    //                 'You\'re right! It\'s Tae Kwon Do now.' ],
-    //                 [ 'Ready for some more interesting questions?',
-    //                 'Let\'s go!',
-    //                 'Too bad! ;D' ] ];    
+    var score = 0;
 
-    // // Y/N QUIZ
-    // for(var i = 0; i < yn_qNrs.length; i++) {
+    console.log('user is taking the quiz: ');
+    alert('Alright, we\'ll start with some yes or no questions.');
 
-    //     console.log(yn_qNrs[i][0]);     // log question
+    // Q's and A's for Y/N quiz
+    // questions [i, 0], true response [i, 1], false response [i, 2]
+    var yn_qNrs = [ [ 'I\'m 25 years old',                   // question 1
+                    'That\'s right!',                         // response 1 if answer === yes
+                    'I am!',                                 // response 1 if answer === no
+                    'true' ],                                // is the yes answer correct?
+                    [ 'I go hiking with my cat.',
+                    'His name is Oreo!',
+                    'You\'re right! I bring my dog too.',
+                    'true' ],
+                    [ 'When I was 7 I told my mom I wanted to be an engineer.',
+                    'She wishes I did. I told her I\'d become an entomologyst ^^',
+                    'Correct!',
+                    'false' ],
+                    [ 'I practice Muay Thai.',
+                    'Not anymore :(',
+                    'You\'re right! It\'s Tae Kwon Do now.',
+                    'false'],
+                    [ 'Ready for some more interesting questions?',
+                    'Let\'s go!',
+                    'Too bad! ;D',
+                    'true' ] ];    
 
-    //     var invalidAnswer = false;
-    //     do {
-    //         if(invalidAnswer) {
-    //             alert('"yes", \'y\', "no", or \'n\' answers only please (case nonsensitive).');
-    //         }
+    // Y/N QUIZ
+    for(var i = 0; i < yn_qNrs.length; i++) {
 
-    //         var answer = prompt(yn_qNrs[i][0]);
-    //         answer = answer.toLowerCase();
-    //         invalidAnswer = !(answer === 'yes' || answer === 'no' || answer ==='y'|| answer === 'n');  // if the answer is neither yes nor no, it's invalid
+        console.log(yn_qNrs[i][0]);     // log question
 
-    //     } while(invalidAnswer);
-    //     console.log('user: ' + answer);
+        var invalidAnswer = false;
+        do {
+            if(invalidAnswer) {
+                alert('"yes", \'y\', "no", or \'n\' answers only please (case nonsensitive).');
+            }
 
-    //     if(answer === 'yes' || answer === 'y') {
-    //         console.log(yn_qNrs[i][1]);
-    //         alert(yn_qNrs[i][1]);
-    //     }
-    //     else {
-    //         console.log(yn_qNrs[i][2]);
-    //         alert(yn_qNrs[i][2]);
-    //     }
-    // }   // Y/N QUIZ END
+            var answer = prompt(yn_qNrs[i][0]);
+            answer = answer.toLowerCase();
+            invalidAnswer = !(answer === 'yes' || answer === 'no' || answer ==='y'|| answer === 'n');  // if the answer is neither yes nor no, it's invalid
 
-    // // NUMBER GUESSING GAME
-    // alert('Welcome to the number guessing game!');
-    // var datNum = Math.floor((Math.random() * 25) + 1);
-    // for(var i = 4; i > 0; i--) {
+        } while(invalidAnswer);
+        console.log('user: ' + answer);
 
-    //     var more = " more";
-    //     if(i === 4) { more = ''; }
-    //     alert('You have ' + i + more + " guesses.");
+        if(answer === 'yes' || answer === 'y') {
+            console.log(yn_qNrs[i][1]);
+            alert(yn_qNrs[i][1]);
+        }
+        else {
+            console.log(yn_qNrs[i][2]);
+            alert(yn_qNrs[i][2]);
+        }
+        if(yn_qNrs[i][3] === "true") {
+            score++;
+        }
+    }   // Y/N QUIZ END
 
-    //     var numGuess = parseFloat(prompt('I\'m thinking of a whole number from 1 to my age...'));
-    //     if((numGuess %1 !== 0) || !(numGuess >= 1 && numGuess <= 25)) {
-    //         alert('Not a valid guess.');
-    //         console.log('in numGuesser: !numGuess.isInteger = ' + (numGuess %1 !== 0));
-    //         console.log('in numGuesser: (numGuess < 1 && numGuess > 25) = ' + (numGuess <= 1 && numGuess >= 25));
-    //         i++;
-    //         continue;
-    //     } 
-    //     else if(numGuess === datNum) {
-    //         alert('You did it! ' + datNum + 'was the correct number.');
-    //         break;
-    //     }
-    //     else if(numGuess > datNum) {
-    //         alert('Too high.');
-    //     }
-    //     else if(numGuess < datNum) {
-    //         alert('Too low.');
-    //     }
-    //     if(i === 1) {
-    //     alert("The number was " + datNum + ".");
-    //     }
-    // } // END OF NUM GUESSING GAME
+    // NUMBER GUESSING GAME
+    alert('Welcome to the number guessing game!');
+    var datNum = Math.floor((Math.random() * 25) + 1);
+    for(var i = 4; i > 0; i--) {
+
+        var more = " more";
+        if(i === 4) { more = ''; }
+        alert('You have ' + i + more + " guesses.");
+
+        var numGuess = parseFloat(prompt('I\'m thinking of a whole number from 1 to my age...'));
+        if((numGuess %1 !== 0) || !(numGuess >= 1 && numGuess <= 25)) {
+            alert('Not a valid guess.');
+            console.log('in numGuesser: !numGuess.isInteger = ' + (numGuess %1 !== 0));
+            console.log('in numGuesser: (numGuess < 1 && numGuess > 25) = ' + (numGuess <= 1 && numGuess >= 25));
+            i++;
+            continue;
+        } 
+        else if(numGuess === datNum) {
+            alert('You did it! ' + datNum + 'was the correct number.');
+            score++;
+            break;
+        }
+        else if(numGuess > datNum) {
+            alert('Too high.');
+        }
+        else if(numGuess < datNum) {
+            alert('Too low.');
+        }
+        if(i === 1) {
+        alert("The number was " + datNum + ".");
+        }
+    } // END OF NUM GUESSING GAME
 
     // MULTIPLE ANSWER QUESTION
     var trueAnswers = [ [ 'entomologyst', 'park ranger', 'soldier',
@@ -117,11 +129,13 @@ if(want){
         else if (trueAnswers[1].indexOf(guess) < 6){
             alert("You got it! I wanted to be a " + guess + " when i was " + trueAnswers[1][trueAnswers[0].indexOf(guess)]);
             console.log('User guessed correctly!');
+            score++;
             break;
         }
         else {
             alert("You got it! I want to be a " + guess);
             console.log('User guessed correctly!');
+            score++;
             break;
         }
         if(i === 1) {
@@ -130,6 +144,9 @@ if(want){
             break;
         }
     } // END OF MULT ANSWER QUESTION
+
+    alert("You got " + score + " out of 7 questions right. Thanks for playing!");
+    console.log("Final score: " + score "/7");
 }
 
 /* FUNCTIONS */
