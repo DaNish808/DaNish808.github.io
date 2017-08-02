15ca6f4c078a25dd6c46b19be9bd6a1ac6fd6e6c
David Nishikawa <triptype@gmail.com>
2017-08-01 17:08:04 -0700

    added content to .html and .css; general outline of page is set; minor changes to .js

--- project/app.js
+++ project/app.js
@@ -1,54 +1,77 @@
 "use strict";
 
-var questionnaire = function(tNf_qNrs, gen_qNrs) {
+
+var questionnaire = function(yn_qNrs, gen_qNrs) {
 
     var want = confirm("Feel like trying a get-to-know-the-author quiz?");
 
     if(want){
+        console.log('user is taking the quiz: ');
         alert('Alright, we\'ll start with some true or false.');
-        trueFalseQuestions(tNf_qNrs);
+        trueFalseQuestions(yn_qNrs);
     }
 }
 
  
 
-var trueFalseQuestions = function(tNf_qNrs) {       // tNf_qNrs is a 2D array holding arrays of strings with question at 0, true answers at 1, and false answers at 2
-    for(var i = 0; i < tNf_qNrs.length; i++) {
+var trueFalseQuestions = function(yn_qNrs) {       // yn_qNrs is a 2D array holding arrays of strings with question at 0, true answers at 1, and false answers at 2
+    for(var i = 0; i < yn_qNrs.length; i++) {
+
+        console.log(yn_qNrs[i][0]);     // log question
 
         var invalidAnswer = false;
         do {
             if(invalidAnswer) {
-                alert('"true" or "false" only please.');
+                alert('"yes", \'y\', "no", or \'n\' answers only please (case nonsensitive).');
             }
 
-            var answer = prompt(tNf_qNrs[i][0]);
-            invalidAnswer = !(answer === 'true' || answer === 'false');  // if the answer is neither yes nor no, it's invalid
+            var answer = prompt(yn_qNrs[i][0]);
+            answer.toLowerCase();
+            invalidAnswer = !(answer === 'yes' || answer === 'no' || answer ==='y'|| answer === 'n');  // if the answer is neither yes nor no, it's invalid
 
         } while(invalidAnswer);
+        console.log('user: ' + answer);
 
-        if(answer === 'true') {
-            alert(tNf_qNrs[i][1]);
+        if(answer === 'yes' || answer === 'y') {
+            console.log(yn_qNrs[i][1]);
+            alert(yn_qNrs[i][1]);
         }
         else {
-            alert(tNf_qNrs[i][2]);
+            console.log(yn_qNrs[i][2]);
+            alert(yn_qNrs[i][2]);
         }
     }
 }
 
 
+
+// Array for a function that can respond to non t/f questions (to be completed)
 var gen_qNrs = [ ['', 'if answer', 'if answer response', 'else answer'],
                  [], ];
 
+
+
 // questions [i, 0], true response [i, 1], false response [i, 2]
-var tNf_qNrs = [ [ 'I grew up in Oregon.', 
-                   'Nope. Hawaii born and raised :D',
-                   'Mahalo!' ],
+var yn_qNrs = [ [ 'I grew up in Oregon.',                  // question 1
+                   'Nope. Hawaii born and raised :D',       // response 1 if answer === true
+                   'Mahalo!' ],                             // response 1 if answer === false
                  [ 'I go hiking with my cat.',
                    'His name is Oreo!',
                    'You\'re right! I bring my dog too.' ],
-                 [ 'When I was 7 I told my mom I wanted to be an engineer',
+                 [ 'When I was 7 I told my mom I wanted to be an engineer.',
                    'She wishes I did. I told her I\'d become an entomologyst ^^',
-                   'Correct!' ] ];
+                   'Correct!' ],
+                 [ 'I practice Muay Thai.',
+                   'Not anymore :(',
+                   'Correct! OSU doesnt have a Muay Thai group, so it\'s taekwondo now.' ],
+                 [ 'Should I modify this quiz to have more than just true/false questions?',
+                   'I agree! I\'ll get right on it after finishing up the necessities.',
+                   'cool' ]
+                 ];
 
 
-questionnaire(tNf_qNrs, gen_qNrs);
+                
+
+//questionnaire(yn_qNrs, gen_qNrs);
+
+
