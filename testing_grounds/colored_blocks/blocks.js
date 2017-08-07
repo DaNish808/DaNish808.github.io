e4578a3351c795f4f802906947b00f8bfa3cf457
David Nishikawa <triptype@gmail.com>
2017-08-07 00:40:34 -0700

    added new section for idea testing

---
+++ testing_grounds/colored_blocks/blocks.js
@@ -0,0 +1,7 @@
+function test() {
+    var grid = document.getElementById('grid');
+    console.log(grid);
+    console.log(grid.rows[0]);
+    console.log(grid.rows[0]);
+    // .setAttribute('background', 'blue');
+}
