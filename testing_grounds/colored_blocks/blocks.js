e78ed8d7f8631fe92ddc5f845e7d3e89b262be3b
David Nishikawa <triptype@gmail.com>
2017-08-18 19:49:59 -0700

    modifying Projects repo

--- testing_grounds/colored_blocks/blocks.js
+++
@@ -1,7 +0,0 @@
-function test() {
-    var grid = document.getElementById('grid');
-    console.log(grid);
-    console.log(grid.rows[0]);
-    console.log(grid.rows[0]);
-    // .setAttribute('background', 'blue');
-}
